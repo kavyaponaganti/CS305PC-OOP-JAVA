a31bfac3611f3d71658ccc60e4432676510cab05
kavyaponaganti <kavyaponaganti5544@gmail.com>
2025-09-25 21:00:12 +0530

    Add files via upload

---
+++ Lab2P1.java
@@ -0,0 +1,27 @@
+import java.util.lang;
+
+class Lab2P1{
+	public static void main(String... args){
+	Scanner s= new Scanner(System.in);
+	int a;
+	string op;
+	s.nextInt();
+	s.nextString();
+		switch(op){
+		case1 "++":
+			System.out.println("pre inc:"+(++a)+"\t"+"post inc:"(a++));
+			break;
+		case2 "--":
+			System.out.println("pre dec:"+(--a)+"/t"+"pos dec:"(a--));
+			break;
+		case3 "+":
+			a=+a;
+			System.out.println("positive sign bit:"+ (a));
+			break;
+		case4 "-":
+			a=-a;
+			System.out.println(unary -:"+ (a));
+			break;
+		}
+	}
+}	
