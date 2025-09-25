800c99011bd3227dc51fde2af4e675421abc88a6
kavyaponaganti <kavyaponaganti5544@gmail.com>
2025-09-25 21:08:59 +0530

    Add files via upload

---
+++ LAB3/Lab3P2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+Class Lab3P2 {
+    public static void main(String... args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 2;
+	for (int i=2; i<n; i++){
+    int count=0;
+    if (i==z){
+        count++;
+    }
+    for (int j=0; j<i; j++){
+        if (i%j==0)
+            count=0;
+            break;
+        else{
+            count++;
+    }
+    if (count>0)
+        System.out.println (i);
+       }
+    }
+}
