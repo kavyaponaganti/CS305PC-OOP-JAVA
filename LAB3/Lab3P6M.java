86bf5c2c03bb2498e067de2926667b1ff0c1859c
kavyaponaganti <kavyaponaganti5544@gmail.com>
2025-10-20 00:13:13 +0530

    Delete LAB3 directory

--- LAB3/Lab3P6M.java
+++
@@ -1,8 +0,0 @@
-Class Lab3P6M {
-   public static void mai(String...args) {
-        Lab3P62(s = new Lab3P62());
-        l2.SetName("");
-        system.out.println(l2.getName());
-        l2.demonstrate();
-    }
-}
