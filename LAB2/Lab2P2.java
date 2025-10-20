5c47289a31c6fa6f3a9e941f0f0c4da78809431d
kavyaponaganti <kavyaponaganti5544@gmail.com>
2025-10-20 12:11:08 +0530

    Update Lab2P2.java

--- LAB2/Lab2P2.java
+++ LAB2/Lab2P2.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
-class LAB2P2 {
-    public static void main(String...args){
-        Scanner s = new Scanner(System.in);
-        int x = s.nextInt();
-        int y = x;
-        System.out.println("Output is: " + x + + + + + x); // Note: x+++++x is likely a typo, interpreted as x++ + ++x
-        x = y;
-        System.out.println("O/p is: " + (x++ + ++x));
-        x = y;
-        System.out.println("O/p is: " + x + + + + + x ); // Note: x+++++x is likely a typo, interpreted as x++ + ++x
-    }
-}
+class Lab2P2{
+	public static void main(String...args){
+	
+	Scanner s = new Scanner(System.in);
+	int a = s.nextInt();
+	int b = a;
+	System.out.println("Output is:"+ a++ + ++a);
+	a = b;
+	System.out.println("Output is:"+ (a++ + ++a));
+	a = b;
+	System.out.println(a++ + ++a + " is Output");
+	
+	}
+	}
