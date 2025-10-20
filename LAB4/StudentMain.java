959ba6a715b410071335de5ed49770167534d188
kavyaponaganti <kavyaponaganti5544@gmail.com>
2025-10-20 09:29:41 +0530

    Add files via upload

---
+++ LAB4/StudentMain.java
@@ -0,0 +1,11 @@
+package Student;
+import java.util.Scanner;
+import java.io.*;
+class StudentMain implements Operations{
+	public static void main(String...args){
+	       String id , branch , password,name;
+	 	Scanner sc = new Scanner(System.in);
+
+
+	}
+}
