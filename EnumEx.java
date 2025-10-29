90d823d8bcb49e90e164e5d6d19fa07d2c5b5441
kavyaponaganti <kavyaponaganti5544@gmail.com>
2025-10-29 10:06:35 +0530

    Add files via upload

---
+++ EnumEx.java
@@ -0,0 +1,10 @@
+enum Fruits{
+        Apple,Banana,Citrus,DragonFruit,Fig,Grapes
+}
+public class EnumEx{
+       public static void main(String...args){
+         	 for(Fruits f: Fruits.values()){
+         	      System.out.println(f);
+         	      }
+         	}
+}         	              	
